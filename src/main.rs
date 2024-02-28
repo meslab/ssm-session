482e1f727c61b4d42fbb2ea57d7c10bbc3ebb371
Anton Sidorov <antosid@gmail.com>
2024-02-28 11:13:39 +0000

    MESLAB-0004: Update version to 0.0.2

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ use tokio;
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
 #[clap(
-    version = "v0.0.1",
+    version = "v0.0.2",
     author = "Anton Sidorov tonysidrock@gmail.com",
     about = "Counts wwords frequency in a text file"
 )]
